bd18bf9f5c2fd65743384f353a014e078b76418c
Developer-AbhinavAF <abhinavdeveloper6@gmail.com>
2026-02-27 13:11:31 +0530

    last commit

--- src/server.js
+++ src/server.js
@@ -5,9 +5,36 @@ const http = require('http');
 const { Server } = require('socket.io');
 const { configureCloudinary } = require('./config/cloudinary');
 
+function runStartupProgressBar(durationMs = 6000) {
+  const width = 24;
+  const start = Date.now();
+
+  return new Promise((resolve) => {
+    const t = setInterval(() => {
+      const elapsed = Date.now() - start;
+      const pct = Math.min(1, elapsed / durationMs);
+      const filled = Math.round(pct * width);
+      const bar = `${'█'.repeat(filled)}${'░'.repeat(Math.max(0, width - filled))}`;
+      const label = String(Math.round(pct * 100)).padStart(3, ' ');
+
+      process.stdout.write(`\rStarting ${label}% [${bar}]`);
+
+      if (pct >= 1) {
+        clearInterval(t);
+        process.stdout.write('\n');
+        resolve();
+      }
+    }, 120);
+  });
+}
+
 async function start() {
-  await connectDb();
-  configureCloudinary();
+  const init = (async () => {
+    await connectDb();
+    configureCloudinary();
+  })();
+
+  await Promise.all([runStartupProgressBar(6000), init]);
 
   const server = http.createServer(app);
   const io = new Server(server, {
